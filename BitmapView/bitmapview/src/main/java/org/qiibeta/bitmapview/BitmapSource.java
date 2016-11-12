21eb27f6c3f68fc29aad3cc2bc96d3deabe8407f
qii <qiibeta@gmail.com>
2016-11-12 23:11:42 +0800

    update sample gradle version and refactor BitmapSource code

--- BitmapView/bitmapview/src/main/java/org/qiibeta/bitmapview/BitmapSource.java
+++ BitmapView/bitmapview/src/main/java/org/qiibeta/bitmapview/BitmapSource.java
@@ -1,6 +1,7 @@
 package org.qiibeta.bitmapview;
 
 
+import android.content.ContentResolver;
 import android.graphics.Bitmap;
 import android.net.Uri;
 
@@ -10,26 +11,35 @@ import org.qiibeta.bitmapview.image.GifImage;
 import org.qiibeta.bitmapview.image.TileImage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class BitmapSource {
+public abstract class BitmapSource {
+    private static final String FILE_SCHEME = "file";
 
     public static BitmapSource newInstance(Uri uri, int thumbnailOrientation, Bitmap thumbnailBitmap) {
-        BitmapSource source = new BitmapSource();
-
-        String path = uri.getPath();
-        if (ImageFormatUtility.isGif(path)) {
-            File file = new File(path);
-            source.mIsGif = file.length() <= 4 * 1024 * 1024;//only support 4mb gif
+        String scheme = uri.getScheme();
+        if (FILE_SCHEME.equals(scheme)) {
+            return new FileSource(uri, thumbnailOrientation, thumbnailBitmap);
         } else {
-            source.mFullOrientation = OrientationInfoUtility.getOrientation(path);
-            source.mFilePath = path;
+            return new InputStreamSource(uri, thumbnailOrientation, thumbnailBitmap);
         }
+    }
 
-        source.uri = uri;
-        source.mThumbnailOrientation = thumbnailOrientation;
-        source.mThumbnailBitmap = thumbnailBitmap;
-        return source;
+    public static BitmapSource newInstance(Bitmap thumbnailBitmap) {
+        return newInstance(0, thumbnailBitmap);
+    }
+
+    public static BitmapSource newInstance(int thumbnailOrientation, Bitmap thumbnailBitmap) {
+        return new SimpleBitmapSource(thumbnailOrientation, thumbnailBitmap);
+    }
+
+    public static BitmapSource newInstance(Bitmap thumbnailBitmap, String filePath) {
+        return newInstance(0, thumbnailBitmap, filePath);
+    }
+
+    public static BitmapSource newInstance(int thumbnailOrientation, Bitmap thumbnailBitmap, String filePath) {
+        return new FileSource(Uri.fromFile(new File(filePath)), thumbnailOrientation, thumbnailBitmap);
     }
 
     public static BitmapSource newInstance(Uri uri, Bitmap thumbnailBitmap) {
@@ -41,45 +51,137 @@ public class BitmapSource {
     }
 
     public static BitmapSource newInstance(Uri uri, int thumbnailOrientation, Bitmap thumbnailBitmap, int fullPictureOrientation, InputStream fullPictureInputStream) {
-        BitmapSource source = new BitmapSource();
-        source.uri = uri;
-        source.mThumbnailOrientation = thumbnailOrientation;
-        source.mFullOrientation = fullPictureOrientation;
-        source.mThumbnailBitmap = thumbnailBitmap;
-        source.mFullPictureInputStream = fullPictureInputStream;
-        return source;
+        return new InputStreamSource(uri, thumbnailOrientation, thumbnailBitmap, fullPictureOrientation, fullPictureInputStream);
     }
 
-    private Uri uri;
-    private int mThumbnailOrientation;
-    private int mFullOrientation;
-    private Bitmap mThumbnailBitmap;
-    private InputStream mFullPictureInputStream;
-    private String mFilePath;
-    private boolean mIsGif;
+    public abstract Uri getUri();
 
-    private BitmapSource() {
-        //empty
+    public abstract AppImage getBitmapImage();
+
+    public abstract TileImage getTileImage();
+
+    private static class SimpleBitmapSource extends BitmapSource {
+        private int mThumbnailOrientation;
+        private Bitmap mThumbnailBitmap;
+
+        public SimpleBitmapSource(int thumbnailOrientation, Bitmap bitmap) {
+            this.mThumbnailOrientation = thumbnailOrientation;
+            this.mThumbnailBitmap = bitmap;
+        }
+
+        @Override
+        public Uri getUri() {
+            return null;
+        }
+
+        @Override
+        public AppImage getBitmapImage() {
+            return BitmapImage.newInstance(mThumbnailOrientation, mThumbnailBitmap);
+        }
+
+        @Override
+        public TileImage getTileImage() {
+            return null;
+        }
     }
 
-    Uri getUri() {
-        return this.uri;
+    //todo 支持gif 支持方向 获取contentResolver
+    private static class InputStreamSource extends BitmapSource {
+        private Uri mUri;
+        private int mThumbnailOrientation;
+        private int mFullOrientation;
+        private Bitmap mThumbnailBitmap;
+        private InputStream mFullPictureInputStream;
+
+        private InputStreamSource(Uri uri, int thumbnailOrientation, Bitmap thumbnailBitmap) {
+            ContentResolver contentResolver = null;
+            try {
+                this.mFullPictureInputStream = contentResolver.openInputStream(uri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            this.mFullOrientation = thumbnailOrientation;
+            this.mUri = uri;
+            this.mThumbnailOrientation = thumbnailOrientation;
+            this.mThumbnailBitmap = thumbnailBitmap;
+        }
+
+        private InputStreamSource(Uri uri, int thumbnailOrientation, Bitmap thumbnailBitmap, int fullPictureOrientation, InputStream fullPictureInputStream) {
+            this.mUri = uri;
+            this.mThumbnailOrientation = thumbnailOrientation;
+            this.mThumbnailBitmap = thumbnailBitmap;
+            this.mFullOrientation = fullPictureOrientation;
+            this.mFullPictureInputStream = fullPictureInputStream;
+        }
+
+        public Uri getUri() {
+            return this.mUri;
+        }
+
+        public AppImage getBitmapImage() {
+            return BitmapImage.newInstance(mThumbnailOrientation, mThumbnailBitmap);
+        }
+
+        public TileImage getTileImage() {
+            if (this.mFullPictureInputStream != null) {
+                return TileImage.newInstance(mFullOrientation, mFullPictureInputStream);
+            } else {
+                return null;
+            }
+        }
     }
 
-    AppImage getBitmapImage() {
-        return mIsGif ? GifImage.newInstance(mThumbnailBitmap, uri.getPath()) : BitmapImage.newInstance(mThumbnailOrientation, mThumbnailBitmap);
-    }
+    private static class FileSource extends BitmapSource {
+        private Uri mUri;
+        private int mThumbnailOrientation;
+        private int mFullOrientation;
+        private Bitmap mThumbnailBitmap;
+        private String mFilePath;
+        private boolean mIsGif;
 
-    TileImage getTileImage() {
-        if (mFullPictureInputStream != null) {
-            return TileImage.newInstance(mFullOrientation, mFullPictureInputStream);
-        } else if (mFilePath != null) {
+        private FileSource(Uri fileUri, int thumbnailOrientation, Bitmap thumbnailBitmap) {
+            super();
+            this.mFilePath = fileUri.getPath();
+            if (ImageFormatUtility.isGif(this.mFilePath)) {
+                File file = new File(this.mFilePath);
+                this.mIsGif = file.length() <= 4 * 1024 * 1024;//only support 4mb gif
+            } else {
+                this.mFullOrientation = OrientationInfoUtility.getOrientation(this.mFilePath);
+            }
+
+            this.mUri = fileUri;
+            this.mThumbnailOrientation = thumbnailOrientation;
+            this.mThumbnailBitmap = thumbnailBitmap;
+        }
+
+        private FileSource(Uri fileUri, int thumbnailOrientation, Bitmap thumbnailBitmap, int fullPictureOrientation) {
+            super();
+            this.mFilePath = fileUri.getPath();
+            if (ImageFormatUtility.isGif(this.mFilePath)) {
+                File file = new File(this.mFilePath);
+                this.mIsGif = file.length() <= 4 * 1024 * 1024;//only support 4mb gif
+                this.mFullOrientation = 0;
+            } else {
+                this.mFullOrientation = fullPictureOrientation;
+            }
+            this.mUri = fileUri;
+            this.mThumbnailOrientation = thumbnailOrientation;
+            this.mThumbnailBitmap = thumbnailBitmap;
+        }
+
+        public Uri getUri() {
+            return this.mUri;
+        }
+
+        public AppImage getBitmapImage() {
+            return mIsGif ? GifImage.newInstance(mThumbnailBitmap, this.mFilePath) : BitmapImage.newInstance(mThumbnailOrientation, mThumbnailBitmap);
+        }
+
+        public TileImage getTileImage() {
             if (ImageFormatUtility.isGif(mFilePath)) {
                 return null;
             }
             return TileImage.newInstance(mFullOrientation, mFilePath);
-        } else {
-            return null;
         }
     }
 }
--- BitmapView/bitmapview/src/main/java/org/qiibeta/bitmapview/ImageFormatUtility.java
+++ BitmapView/bitmapview/src/main/java/org/qiibeta/bitmapview/ImageFormatUtility.java
@@ -3,11 +3,20 @@ package org.qiibeta.bitmapview;
 
 import android.graphics.BitmapFactory;
 
+import java.io.InputStream;
+
 class ImageFormatUtility {
     static boolean isGif(String path) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
+        return "image/gif".equals(options.outMimeType);
+    }
+
+    static boolean isGif(InputStream inputStream) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
         return "image/gif".equals(options.outMimeType);
     }
 }
